6d1fd61b3fc4e7eebbec3dea6d56660338949700
Scottie Thurwanger <thurwangers@gmail.com>
2021-09-25 21:58:04 -0400

    Lecture 8 complete

--- AT-University/AT-University/Lecture 8/Lecture8.cs
+++ AT-University/AT-University/Lecture 8/Lecture8.cs
@@ -5,10 +5,10 @@ using OpenQA.Selenium.Chrome;
 namespace AT_University
 {
     [TestClass]
-    public class example
+    public class Lecture8
     {
         private IWebDriver _driver;
-        private readonly string _url = "http://www.google.com";
+        private readonly string _url = "https://pythonspot.com/selenium/";
 
         [TestInitialize]
         public void Setup()
@@ -17,10 +17,14 @@ namespace AT_University
         }
 
         [TestMethod]
-        public void AbleToSearchGoogle()
+        public void TitleIsCorrect()
         {
             _driver.Navigate().GoToUrl(_url);
+            var title = _driver.FindElement(By.ClassName("post-title")).Text;
+            Assert.AreEqual(title, "Selenium - Web Automation with Python");
+
         }
+
 
         [TestCleanup]
         public void Quit()
