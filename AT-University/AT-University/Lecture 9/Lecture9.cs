6702cf905d4d9ff8931a456494e55ce719c53e3d
Scottie Thurwanger <thurwangers@gmail.com>
2021-09-30 23:44:43 -0400

    Lecture 9 complete

---
+++ AT-University/AT-University/Lecture 9/Lecture9.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AT_University
+{
+    [TestClass]
+    public class Lecture9
+    {
+        private IWebDriver _driver;
+        private readonly string _url = "https://pythonspot.com/selenium/";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void FindElementByDifferentMethods()
+        {
+            _driver.Navigate().GoToUrl(_url);
+            var link = _driver.FindElement(By.LinkText("Selenium install"));
+            link.Click();
+            var Id = _driver.FindElement(By.Id("contact-form-name"));
+            Id.Click();
+            Id.SendKeys("Samuel");
+            var Name = _driver.FindElement(By.Name("email"));
+            Name.Click();
+            Name.SendKeys("test@at.edu");
+
+        }
+
+
+        [TestCleanup]
+        public void Quit()
+        {
+            _driver.Quit();
+        }
+    }
+}
